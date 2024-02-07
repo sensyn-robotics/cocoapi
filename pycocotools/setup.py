dc70c3b3bee4e16ca527acce3ebebabc088f0761
Se66ie <100899141+Se66ie@users.noreply.github.com>
2024-02-07 18:07:33 +0900

    fix_cpython version

--- pycocotools/setup.py
+++ pycocotools/setup.py
@@ -22,7 +22,7 @@ setup(name='mmpycocotools',
       packages=['pycocotools'],
       package_dir={'pycocotools': 'pycocotools'},
       install_requires=[
-          'setuptools>=18.0', 'cython>=0.27.3', 'matplotlib>=2.1.0'
+          'setuptools>=18.0', 'cython==0.29.36', 'matplotlib>=2.1.0'
       ],
       version='12.0.3',
       ext_modules=ext_modules)
